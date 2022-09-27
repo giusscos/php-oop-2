881894878ce5242c493429938891fc3f62612cf0
giusscos <giu.cosenza08@gmail.com>
2022-09-27 16:04:09 +0200

    Aggiunta Classe Prodotto

---
+++ classes/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+class Product
+{
+    public $name;
+    public $desc;
+    public $price;
+    public $brand;
+
+
+    function __construct($param)
+    {
+        $this->name = $param["name"];
+        $this->desc = $param["desc"];
+        $this->price = $param["price"];
+        $this->brand = $param["brand"];
+    }
+}
--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-Ciao OOP PHP 2
+<?php
+
+include __DIR__ . '/classes/Product.php';
+
+$data = [
+    "name" => 'Croccantini di pollo',
+    "brand" => 'Sium',
+    "desc" => 'Croccantini per animali fantastici',
+    "price" => 29.00,
+];
+
+$prod1 = new Product($data);
+
+var_dump($prod1);
